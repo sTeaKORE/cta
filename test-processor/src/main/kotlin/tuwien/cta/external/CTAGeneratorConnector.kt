8d19cfe439c189cf80e7f0bdcfc7013b7ac4b5a1
sTeaKore <sTeaKor1337@gmx.at>
2022-07-14 17:53:26 +0200

    start connecting created config with program

--- test-processor/src/main/kotlin/tuwien/cta/external/CTAGeneratorConnector.kt
+++ test-processor/src/main/kotlin/tuwien/cta/external/CTAGeneratorConnector.kt
@@ -4,12 +4,12 @@ import java.io.File
 import java.io.IOException
 import java.util.concurrent.TimeUnit
 
-class CTGeneratorConnector() {
+class CTAGeneratorConnector() {
 
-    private val command = "./fipo-cli -i 2^10 -p"
-
-    fun generateTestSet() {
+    fun generateTestSet(configPath: String) {
+        val command = generateCommand(configPath)
         val directory = getResourceDirectory()
+
         val response = command.runCommand(directory)
         println(response)
     }
@@ -19,18 +19,32 @@ class CTGeneratorConnector() {
         val cliBinaryFile = File(cliBinary.path)
         return cliBinaryFile.parentFile
     }
+
+    private fun generateCommand(configPath: String): String {
+        val binaryPath = this::class.java.classLoader.getResource("fipo-cli")
+        val binaryPathFile = File(binaryPath.path)
+        val binaryPathFilePath = binaryPathFile.absolutePath
+        val outputPath = configPath.getOutputPath()
+        return "$binaryPathFilePath -t 3 -i $configPath -o $outputPath --randomize"
+    }
+
+    companion object {
+        const val CONFIG_FILE_SUFFIX = "ACTSConfig"
+        const val OUTPUT_FILE_SUFFIX = "CAGENTestset"
+        const val TXT_ENDING = "txt"
+        const val CSV_ENDING = "csv"
+    }
 }
 
 fun main() {
-    val connector = CTGeneratorConnector()
-    connector.generateTestSet()
+    val connector = CTAGeneratorConnector()
+    connector.generateTestSet("/home/steakor/uni/master/projekt/workload/build/generated/ksp/test/resources/TestClassACTSConfig.txt")
 }
 
 fun String.runCommand(workingDir: File): String? {
     return try {
         val parts = this.split("\\s".toRegex())
         val proc = ProcessBuilder(*parts.toTypedArray())
-            .directory(workingDir)
             .redirectOutput(ProcessBuilder.Redirect.PIPE)
             .redirectError(ProcessBuilder.Redirect.PIPE)
             .start()
@@ -42,3 +56,11 @@ fun String.runCommand(workingDir: File): String? {
         null
     }
 }
+
+fun String.getOutputPath(): String = this.reversed().replaceFirst(
+    CTAGeneratorConnector.CONFIG_FILE_SUFFIX.reversed(),
+    CTAGeneratorConnector.OUTPUT_FILE_SUFFIX.reversed()
+).replaceFirst(
+    CTAGeneratorConnector.TXT_ENDING.reversed(),
+    CTAGeneratorConnector.CSV_ENDING.reversed()
+).reversed()
--- test-processor/src/main/kotlin/tuwien/cta/visitors/TestDeclarationVisitor.kt
+++ test-processor/src/main/kotlin/tuwien/cta/visitors/TestDeclarationVisitor.kt
@@ -4,12 +4,13 @@ import com.google.devtools.ksp.processing.CodeGenerator
 import com.google.devtools.ksp.processing.Dependencies
 import com.google.devtools.ksp.processing.Resolver
 import com.google.devtools.ksp.symbol.*
-import tuwien.cta.external.CTGeneratorConnector
+import tuwien.cta.external.CTAGeneratorConnector
 import tuwien.cta.template.createTestTemplate
 import tuwien.cta.util.LoggingUtil
 import tuwien.cta.util.appendText
 import tuwien.cta.input_model.CTAInputModel
 import tuwien.cta.template.createACTSemplate
+import java.io.FileNotFoundException
 
 class TestDeclarationVisitor(
     private val codeGenerator: CodeGenerator,
@@ -18,7 +19,7 @@ class TestDeclarationVisitor(
 ): KSVisitorVoid() {
 
     private val knownAnnotations = listOf("CTATest")
-    private val generatorConnector = CTGeneratorConnector()
+    private val generatorConnector = CTAGeneratorConnector()
 
     override fun visitFunctionDeclaration(function: KSFunctionDeclaration, data: Unit) {
         if (!visited.contains(function.simpleName.asString())) {
@@ -52,18 +53,26 @@ class TestDeclarationVisitor(
                 var inputModel = CTAInputModel(argumentDeclaration.simpleName.asString())
                 inputModel = argumentDeclaration.accept(ConstraintAnnotationVisitor(codeGenerator, resolver, loggingUtil), inputModel)
                 loggingUtil.log("Received Input Model:\n$inputModel")
-                generateACTSFile(inputModel)
-                generatorConnector.generateTestSet()
+                val pathToACTSFile = generateACTSFile(inputModel)
+                generatorConnector.generateTestSet(pathToACTSFile)
             }
         }
     }
 
-    private fun generateACTSFile(inputModel: CTAInputModel) {
-        val filename = "${inputModel.systemName}ACTSConfig"
+    private fun generateACTSFile(inputModel: CTAInputModel): String {
+        val filename = "${inputModel.systemName}${CTAGeneratorConnector.CONFIG_FILE_SUFFIX}"
         val file = codeGenerator.createNewFile(Dependencies(false), "", filename, "txt")
+
         val actsTemplate = createACTSemplate(inputModel)
         file.appendText(actsTemplate)
         file.close()
+
+        val configFile = codeGenerator.generatedFile.find { it.name == "$filename.txt" }
+        if (configFile != null) {
+            return configFile.absolutePath
+        } else {
+            throw FileNotFoundException("Generated Config File not found")
+        }
     }
 
     companion object {
